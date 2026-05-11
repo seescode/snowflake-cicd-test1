0cd1f2eda030367d14fca0e1e36dd22a50191ace
seescode <yongc.programmer@gmail.com>
2026-05-10 15:31:00 -1000

    Fix path bug

--- snowflake/deploy.sql
+++ snowflake/deploy.sql
@@ -1,10 +1,10 @@
 -- Set context
 USE WAREHOUSE <%wh%>;
 
-!source ./tables/claims.sql
-!source ./tables/members.sql
-!source ./tables/providers.sql
+!source tables/claims.sql
+!source tables/members.sql
+!source tables/providers.sql
 
-!source ./views/vw_claims_summary.sql
+!source views/vw_claims_summary.sql
 
-!source ./procedures/process_claim.sql
+!source procedures/process_claim.sql
