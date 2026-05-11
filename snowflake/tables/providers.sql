26e17d1d3c953458b7c97435144790d734da05b6
seescode <yongc.programmer@gmail.com>
2026-05-10 15:12:01 -1000

    Update table creation syntax

--- snowflake/tables/providers.sql
+++ snowflake/tables/providers.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE providers (
+CREATE TABLE providers (
     provider_id     VARCHAR(36)     NOT NULL,
     first_name      VARCHAR(100)    NOT NULL,
     last_name       VARCHAR(100)    NOT NULL,
